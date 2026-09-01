8dee87d13fcec534cd40b2963912ef9f8b315bf0
ecknight <ellyknight@gmail.com>
2026-09-01 08:35:12 -0600

    Explore across thresholds

--- call_rate/03_evaluate.R
+++ call_rate/03_evaluate.R
@@ -11,6 +11,7 @@
 
 ## 1.1 Load packages ----
 library(tidyverse) # data manipulation and visualization (version: 2.0.0)
+library(gridExtra)
 
 ## 1.2 Set root path ----
 root <- "G:/Shared drives/ABMI_Acoustics/Classifiers/HawkEars - Temporal Localization/call_rate"
@@ -26,33 +27,43 @@ table(dat$outcome, dat$type)
 ## 2.2. Summarize to minute ----
 unverif <- dat |>
   group_by(file, type) |>
-  summarize(calls_unverified = n()) |>
+  summarize(calls_unverified = n(),
+            duration_unverified = sum(duration.classifier)) |>
   ungroup()
 
 verif <- dat |>
   dplyr::filter(outcome == "true positive") |>
   group_by(file, type) |>
-  summarize(calls_verified = n()) |>
+  summarize(calls_verified = n(),
+            duration_verified = sum(duration.classifier)) |>
   ungroup()
 
-annotated <- dat |>
+annotated_1 <- dat |>
   dplyr::filter(!is.na(start.manual)) |>
   group_by(file) |>
-  summarize(calls_annotated = n()) |>
+  summarize(calls_annotated = n(),
+            duration_annotated = sum(duration.manual)) |>
   ungroup()
 
+annotated <- annotated_1 |> 
+  mutate(type="fixed") |> 
+  rbind(annotated_1 |> 
+          mutate(type="variable"))
+
 recs <- annotated |>
-  left_join(unverif, by = "file") |>
+  left_join(unverif, by = c("file", "type")) |>
   left_join(verif, by = c("file", "type")) |>
   mutate(
     calls_unverified = ifelse(is.na(calls_unverified), 0, calls_unverified),
-    calls_verified = ifelse(is.na(calls_verified), 0, calls_verified)
+    calls_verified = ifelse(is.na(calls_verified), 0, calls_verified),
+    duration_unverified = ifelse(is.na(duration_unverified), 0, duration_unverified),
+    duration_verified = ifelse(is.na(duration_verified), 0, duration_verified)
   ) |>
-  dplyr::select(file, type, calls_annotated, calls_unverified, calls_verified)
+  dplyr::select(file, type, calls_annotated, calls_unverified, calls_verified, duration_annotated, duration_unverified, duration_verified)
 
 ## 2.3 Visalize ----
 
-ggplot(recs) +
+plot.calls.unverified <- ggplot(recs) +
   geom_point(aes(x = calls_annotated, y = calls_unverified, color = type)) +
   geom_smooth(
     aes(x = calls_annotated, y = calls_unverified, color = type),
@@ -60,13 +71,16 @@ ggplot(recs) +
   ) +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed")
 
-ggplot(recs) +
+plot.calls.verified <- ggplot(recs) +
   geom_point(aes(x = calls_annotated, y = calls_verified, color = type)) +
   geom_smooth(
     aes(x = calls_annotated, y = calls_verified, color = type),
     method = "lm"
   ) +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed")
+
+ggsave(grid.arrange(plot.calls.unverified, plot.calls.verified, ncol=2),
+      file=file.path(root, "figures", "Calls_AllData.jpeg"), width = 10, height = 4)
 
 ## 2.4 Correlation ----
 fixed <- recs |>
@@ -78,3 +92,93 @@ variable <- recs |>
   dplyr::filter(type == "variable")
 cor(variable$calls_annotated, variable$calls_unverified, method = "pearson")
 cor(variable$calls_annotated, variable$calls_verified, method = "pearson")
+
+## 2.5 Duration ----
+#this was from when we didn't set a minimum duration
+# ggplot(dat |> 
+#          dplyr::filter(type=="variable")) + geom_histogram(aes(x=duration.classifier, fill=outcome))
+# 
+# ggsave(file.path(root, "figures", "VariableDuration.jpeg"), width = 6, height = 4)
+
+plot.duration.unverified <- ggplot(recs) + 
+  geom_point(aes(x = duration_annotated, y = duration_unverified, color = type)) +
+  geom_smooth(
+    aes(x = duration_annotated, y = duration_unverified, color = type),
+    method = "lm"
+  ) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed")
+
+plot.duration.verified <- ggplot(recs) + 
+  geom_point(aes(x = duration_annotated, y = duration_verified, color = type)) +
+  geom_smooth(
+    aes(x = duration_annotated, y = duration_verified, color = type),
+    method = "lm"
+  ) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed")
+
+ggsave(grid.arrange(plot.duration.unverified, plot.duration.verified, ncol=2),
+       file=file.path(root, "figures", "Duration_AllData.jpeg"), width = 10, height = 4)
+
+
+# 3. Summmarize at varying thresholds ----
+
+## 3.1 Summarize ----
+thresh <- seq(0.1, 0.99, 0.01)
+
+out.list <- list()
+for(i in 1:length(thresh)){
+  
+  dat.i <- dplyr::filter(dat, score > thresh[i])
+  
+  unverif.i <- dat.i |>
+    group_by(file, type) |>
+    summarize(calls_unverified = n(),
+              duration_unverified = sum(duration.classifier)) |>
+    ungroup()
+  
+  verif.i <- dat.i |>
+    dplyr::filter(outcome == "true positive") |>
+    group_by(file, type) |>
+    summarize(calls_verified = n(),
+              duration_verified = sum(duration.classifier)) |>
+    ungroup()
+  
+  recs.i <- annotated |>
+    left_join(unverif.i, by = c("file", "type")) |>
+    left_join(verif.i, by = c("file", "type")) |>
+    mutate(
+      calls_unverified = ifelse(is.na(calls_unverified), 0, calls_unverified),
+      calls_verified = ifelse(is.na(calls_verified), 0, calls_verified),
+      duration_unverified = ifelse(is.na(duration_unverified), 0, duration_unverified),
+      duration_verified = ifelse(is.na(duration_verified), 0, duration_verified)
+    ) |>
+    dplyr::select(file, type, calls_annotated, calls_unverified, calls_verified, duration_annotated, duration_unverified, duration_verified) |> 
+    mutate(thresh = thresh[i])
+  
+  out.list[[i]] <- recs.i
+  
+  cat(i, " ")
+  
+}
+
+out <- do.call(rbind, out.list)
+
+## 3.2 Get the correlations ----
+out.cor <- out |> 
+  group_by(type, thresh) |> 
+  summarize(cor_calls_verified = cor(calls_annotated, calls_verified, method="pearson"),
+            cor_calls_unverified = cor(calls_annotated, calls_unverified, method="pearson"),
+            cor_duration_verified = cor(duration_annotated, duration_verified, method="pearson"),
+            cor_duration_unverified = cor(duration_annotated, duration_unverified, method="pearson")) |> 
+  ungroup() |> 
+  pivot_longer(cor_calls_verified:cor_duration_unverified, names_to="metric", values_to="correlation") |> 
+  mutate(metric = str_remove(metric, "cor_")) |> 
+  separate(metric, into=c("metric", "verification"))
+
+## 3.3 Plot ----
+ggplot(out.cor |> 
+         dplyr::filter(thresh > 0.2), aes(x=thresh, y=correlation)) +
+  geom_line(aes(colour=type)) +
+  facet_grid(metric ~ verification, scales="free")
+
+ggsave(file = file.path(root, "figures", "Correlations.jpeg"), width = 10, height = 8)
